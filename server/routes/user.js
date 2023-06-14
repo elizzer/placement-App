453d48036b8094c3fa32dce50ab33b43549bbe30
elizzer <64605932+elizzer@users.noreply.github.com>
2023-06-14 23:24:31 +0530

    user login, jwt

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,12 @@ const express = require("express")
 
 const router =  express.Router()
 
-const {login,register}= require('../controller/user')
+const {login,register,requiredSignin}= require('../controller/user')
 
 router.post("/login",login)
 
 router.post("/register",register)
+
+router.get("/get",requiredSignin)
 
 module.exports=router
